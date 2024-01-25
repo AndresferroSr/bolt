5e6d5e65ef014408393ce396398881286a58a35f
AndresferroSr <156261305+AndresferroSr@users.noreply.github.com>
2024-01-25 05:07:57 +0000

    new feature

--- backend/task2/eda.py
+++ backend/task2/eda.py
@@ -3,7 +3,7 @@ from ydata_profiling import ProfileReport
 
 
 try:
-    df = pd.read_excel('Dataset Task 2.xlsx')
+    df = pd.read_excel('Dataset Task 2.xlsx', sheet_name = "Data")
     print(df.shape)
     profile = ProfileReport(df, title="Profiling Report", explorative=True)
     profile.to_file("your_report_task2.html")
