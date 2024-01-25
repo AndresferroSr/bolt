225d509441078be884c06df42b06d0145933d328
AndresferroSr <156261305+AndresferroSr@users.noreply.github.com>
2024-01-25 04:48:52 +0000

    test task2

---
+++ backend/task2/eda.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+
+try:
+    df = pd.read_excel('Dataset Task 2.xlsx')
+    print(df.shape)
+    profile = ProfileReport(df, title="Profiling Report", explorative=True)
+    profile.to_file("your_report_task2.html")
+except Exception as e:
+    print(e)
+    breakpoint()
